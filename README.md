5ad32be8bbd94287115fb856428d08fdb454b5d9
Pablo Pedraza <pabloj.pedraza@gmail.com>
2024-11-05 10:39:11 -0300

    agrego ejercicio 5

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Mis soluciones para los [Retos de programación terrorificos con JavaScript](htt
 | 2   | [Horda de zombies](https://www.halloween.dev/es/retos/2024/2)                 | [solución](./src/challenge02.js) | <img src="./images/image-2.png" width="50" height="50" /> | 280    |
 | 3   | [Escapa de la pesadilla de Freddy](https://www.halloween.dev/es/retos/2024/3) | [solución](./src/challenge03.js) | <img src="./images/image-3.png" width="50" height="50" /> | 240    |
 | 4   | [Encuentra al asesino](https://www.halloween.dev/es/retos/2024/4)             | [solución](./src/challenge04.js) | <img src="./images/image-4.png" width="50" height="50" /> | 400    |
+| 5   | [Encuentra al asesino](https://www.halloween.dev/es/retos/2024/5)             | [solución](./src/challenge05.js) | <img src="./images/image-5.png" width="50" height="50" /> | 25     |
 
 ## Como ejecutar el código :running:
 
--- src/challenge05.js
+++ src/challenge05.js
@@ -1,36 +1,81 @@
-function cyberReindeer(road, time) {
-  const output = [road]
-
-  let lastChar = '.'
-  for (let iteration = 1; iteration < time; iteration++) {
-    if (iteration === 5) road = road.replaceAll('|', '*')
-
-    const matches = road.match(/S[\*\.]/g)
-    if (matches) {
-      road = road.replace(matches[0], `${lastChar}S`)
-      lastChar = matches[0][1]
+function escapePyramidHead(room) {
+  const findPosition = (target) => {
+    const rowIndex = room.findIndex((row) => row.includes(target))
+    if (rowIndex !== -1) {
+      const columnIndex = room[rowIndex].indexOf(target)
+      return { x: rowIndex, y: columnIndex }
+    } else {
+      return { x: -1, y: -1 }
     }
-    output.push(road)
   }
 
-  return output
+  const maxRow = room.length - 1
+  const maxCol = room[0].length - 1
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+  ]
+
+  const findPath = (fromPosition, toPosition, steps, visited) => {
+    if (
+      fromPosition.x < 0 ||
+      fromPosition.x > maxRow ||
+      fromPosition.y < 0 ||
+      fromPosition.y > maxCol ||
+      room[fromPosition.x][fromPosition.y] === '#' ||
+      visited.has(`${fromPosition.x},${fromPosition.y}`)
+    ) {
+      return Infinity
+    }
+
+    if (fromPosition.x === toPosition.x && fromPosition.y === toPosition.y) {
+      return steps
+    }
+
+    visited.add(`${fromPosition.x},${fromPosition.y}`)
+
+    let minSteps = Infinity
+    for (const [dr, dc] of directions) {
+      const newRow = fromPosition.x + dr
+      const newCol = fromPosition.y + dc
+      minSteps = Math.min(
+        minSteps,
+        findPath({ x: newRow, y: newCol }, toPosition, steps + 1, visited)
+      )
+    }
+
+    return minSteps
+  }
+
+  const result = findPath(findPosition('▲'), findPosition('T'), 0, new Set())
+  return result === Infinity ? -1 : result
 }
 
-const road = 'S..|...|..'
-const time = 10 // unidades de tiempo
-console.log(cyberReindeer(road, time))
-
-/* -> result:
-[
-1  'S..|...|..', // estado inicial
-2  '.S.|...|..', // avanza el trineo la carretera
-3  '..S|...|..', // avanza el trineo la carretera
-4  '..S|...|..', // el trineo para en la barrera
-5  '..S|...|..', // el trineo para en la barrera
-6  '...S...*..', // se abre la barrera, el trineo avanza
-7  '...*S..*..', // avanza el trineo la carretera
-8  '...*.S.*..', // avanza el trineo la carretera
-9  '...*..S*..', // avanza el trineo la carretera
-10  '...*...S..', // avanza por la barrera abierta
+const room = [
+  ['.', '.', '#', '.', '▲'],
+  ['#', '.', '#', '.', '#'],
+  ['.', '.', '.', '.', '.'],
+  ['#', '#', '#', '.', '#'],
+  ['T', '.', '.', '.', '.']
 ]
-*/
+
+console.log(escapePyramidHead(room)) // -> 8
+
+const room2 = [
+  ['T', '.', '#', '.'],
+  ['.', '.', '.', '.'],
+  ['▲', '.', '.', '#'],
+  ['.', '#', '#', '#']
+]
+
+console.log(escapePyramidHead(room2)) // -> 2
+
+const room3 = [
+  ['#', '#', '#'],
+  ['▲', '.', '#'],
+  ['.', '#', 'T']
+]
+
+console.log(escapePyramidHead(room3)) // -> -1
